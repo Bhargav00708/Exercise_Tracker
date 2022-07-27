f696ce37e714519440320fe1b3ccdaf95d7d8b5d
Bhargav00708 <vanrabhargav@gmail.com>
2022-07-27 22:28:55 +0530

    Format changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 app.use(express.static('public'))
 
-var allUserData = []; 
+var allUserData = [];
 var allUserDataWithLogs = [];
 
 app.get('/', (req, res) => {
@@ -18,21 +18,19 @@ app.get('/', (req, res) => {
 
 app.post('/api/users', (req, res) => {
   var uniqID = new Date().getTime();
-  var userNameAndID = {"username":req.body.username,"_id":uniqID};
+  var userNameAndID = { "username": req.body.username, "_id": uniqID };
   res.send(userNameAndID);
   allUserData.push(userNameAndID);
 });
 
 app.get('/api/users', (req, res) => {
-  if(allUserData.length == 0)
-  {
+  if (allUserData.length == 0) {
     return res.send(`No Data`);
   }
   res.send(allUserData)
 });
 
-app.post('/api/users/:_id/exercises',(req,res)=>
-{
+app.post('/api/users/:_id/exercises', (req, res) => {
   var isFlag = false;
   var count = 1;
   var objectDate = new Date(req.body.date);
@@ -41,67 +39,54 @@ app.post('/api/users/:_id/exercises',(req,res)=>
   var id = req.body[":_id"];
   var log = [];
   var username = "";
-  allUserData.map((value)=>
-  {
-    if(id == value["_id"])
-    {
+  allUserData.map((value) => {
+    if (id == value["_id"]) {
       username = value.username;
     }
   })
-  if(username == "")
-  {
+  if (username == "") {
     return res.send(`Invalid ID`);
   }
-  if(objectDate == `Invalid Date`)
-  {
+  if (objectDate == `Invalid Date`) {
     objectDate = new Date().toDateString();
   }
-  allUserDataWithLogs.map((value,index)=>
-  {
-    if(value.count && id == value["_id"])
-    {
+  allUserDataWithLogs.map((value, index) => {
+    if (value.count && id == value["_id"]) {
       isFlag = true;
       count = value.count++;
       log.push(value.log);
-      log.push({"description":description,"duration":parseInt(duration),"date":objectDate})
+      log.push({ "description": description, "duration": parseInt(duration), "date": objectDate })
       allUserDataWithLogs[index].count = count;
       allUserDataWithLogs[index].log = log;
-    } 
+    }
   })
-  if(!isFlag)
-  {
-    log.push({"description":description,"duration":parseInt(duration),"date":objectDate})
-    allUserDataWithLogs.push({"_id":id,"count":count,"log":log})
+  if (!isFlag) {
+    log.push({ "description": description, "duration": parseInt(duration), "date": objectDate })
+    allUserDataWithLogs.push({ "_id": id, "count": count, "log": log })
   }
-  res.send({"_id":id,"username":username,"date":objectDate,"duration":parseInt(duration),"description":description})
+  res.send({ "_id": id, "username": username, "date": objectDate, "duration": parseInt(duration), "description": description })
 })
 
-app.get('/api/users/:_id/logs',(req,res)=>
-{
+app.get('/api/users/:_id/logs', (req, res) => {
   var id = req.params["_id"];
   var username = "";
-  allUserData.map((value)=>
-  {
-    if(id == value["_id"])
-    {
+  allUserData.map((value) => {
+    if (id == value["_id"]) {
       username = value.username;
     }
   })
-  if(username == "")
-  {
+  if (username == "") {
     return res.send(`Invalid ID`);
-  } 
+  }
   var count = 0;
   var log = [];
-  allUserDataWithLogs.map((value,index)=>
-  {
-    if(value["_id"] == id)
-    {
+  allUserDataWithLogs.map((value, index) => {
+    if (value["_id"] == id) {
       count = allUserDataWithLogs[index].count;
       log = allUserDataWithLogs[index].log;
     }
   })
-  res.send({"_id":id,"username":username,"count":count,"log":log})
+  res.send({ "_id": id, "username": username, "count": count, "log": log })
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
